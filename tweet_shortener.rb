2f639408cab5a293983a819c3bec277b7a5f45fa
Nick Inman <nick@theoatmeal.com>
2018-11-23 23:44:52 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+def dictionary
+  subs = {:hi => "hello", :2 => "to","two","too", :4 => "for","four", :b => "be", :u => "you", :@ => "at", :& => "and"}
+end
+
+def word_substituter
+  
+end
