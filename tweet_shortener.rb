34a894ee3807d24f147301f0c977f95590cfe475
Nick Inman <nick@theoatmeal.com>
2018-11-23 23:50:43 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,8 +1,21 @@
 # Write your code here.
 def dictionary
   subs = {
+    "hello" => "hi",
+    "to" => "2", "too" => "2", "two" => "2",
+    "for" => "4", "four" => "4",
+    "be" => "b",
+    "you" => "u"
+    "at" => "@"
+    "and" => "&"
 end
 
-def word_substituter
-  
+def word_substituter(tweet)
+  tweet.split.map do |subs|
+  if dictionary.keys.include?(subs.downcase)
+    subs = dictionary(subs)
+  else
+    subs
+  end
+end.join(" ")
 end
